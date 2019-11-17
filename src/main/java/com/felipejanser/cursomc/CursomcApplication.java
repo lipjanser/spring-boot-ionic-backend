bd341df8e5f74bab48dd11437af5dfee680bbe64
Felipe Janser <felipejanser@gmail.com>
2019-11-17 15:19:43 -0300

    Projeto criado

---
+++ src/main/java/com/felipejanser/cursomc/CursomcApplication.java
@@ -0,0 +1,14 @@
+package com.felipejanser.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Weeey");
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
