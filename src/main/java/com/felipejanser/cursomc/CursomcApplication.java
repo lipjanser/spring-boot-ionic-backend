7abd3a7224eba56eb35654992a86121feeb27d3c
Felipe Janser <felipejanser@gmail.com>
2020-03-16 23:21:03 -0300

    Estado e Cidade

--- src/main/java/com/felipejanser/cursomc/CursomcApplication.java
+++ src/main/java/com/felipejanser/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.felipejanser.cursomc.domain.Categoria;
+import com.felipejanser.cursomc.domain.Cidade;
+import com.felipejanser.cursomc.domain.Estado;
 import com.felipejanser.cursomc.domain.Produto;
 import com.felipejanser.cursomc.repositories.CategoriaRepository;
+import com.felipejanser.cursomc.repositories.CidadeRepository;
+import com.felipejanser.cursomc.repositories.EstadoRepository;
 import com.felipejanser.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -42,6 +50,23 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Pernambuco");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Recife", est1);
+		Cidade c2 = new Cidade(null, "Vitória de Santo Antão", est1);
+		Cidade c3 = new Cidade(null, "Gravatá", est1);
+		
+		Cidade c4 = new Cidade(null, "São Paulo", est2);
+		Cidade c5 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1,c2,c3));
+		est2.getCidades().addAll(Arrays.asList(c4,c5));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3,c4,c5));
+		
 	}
 
 }
