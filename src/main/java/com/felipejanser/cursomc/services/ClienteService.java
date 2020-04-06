5db85ac7f42c2d39ebcfccaac7b955d7def2dba5
Felipe Janser <felipejanser@gmail.com>
2020-04-06 20:01:52 -0300

    Atualizando categoria com PUT, outros ajustes

--- src/main/java/com/felipejanser/cursomc/services/ClienteService.java
+++ src/main/java/com/felipejanser/cursomc/services/ClienteService.java
@@ -15,9 +15,19 @@ public class ClienteService {
 	@Autowired
 	private ClienteRepository repo;
 	
-	public Cliente buscar(Integer id) {
+	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Cliente update(Cliente obj) {
+		find(obj.getId());
+		return repo.save(obj);
 	}
 	
 }
--- src/main/java/com/felipejanser/cursomc/services/PedidoService.java
+++ src/main/java/com/felipejanser/cursomc/services/PedidoService.java
@@ -15,9 +15,19 @@ public class PedidoService {
 	@Autowired
 	private PedidoRepository repo;
 	
-	public Pedido buscar(Integer id) {
+	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+	}
+	
+	public Pedido insert(Pedido obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Pedido update(Pedido obj) {
+		find(obj.getId());
+		return repo.save(obj);
 	}
 	
 }
