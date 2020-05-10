d9bdf6cda965936fea83cff22d7d91d983f432bf
Felipe Janser <felipejanser@gmail.com>
2020-05-10 16:43:34 -0300

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/com/felipejanser/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/felipejanser/cursomc/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.felipejanser.cursomc.domain.Cliente;
+import com.felipejanser.cursomc.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
