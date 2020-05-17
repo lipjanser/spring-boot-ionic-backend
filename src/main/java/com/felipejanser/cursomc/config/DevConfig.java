188da326d3b9c6e53fae803d6f6b930c5c92ed8d
Felipe Janser <felipejanser@gmail.com>
2020-05-17 18:51:46 -0300

    Criando profile de teste

---
+++ src/main/java/com/felipejanser/cursomc/config/DevConfig.java
@@ -0,0 +1,10 @@
+package com.felipejanser.cursomc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+}
