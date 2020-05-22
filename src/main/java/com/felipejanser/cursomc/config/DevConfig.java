5f88ae457baa477cd8040dce6851b734a55a0d87
Felipe Janser <felipejanser@gmail.com>
2020-05-22 10:46:11 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/felipejanser/cursomc/config/DevConfig.java
+++ src/main/java/com/felipejanser/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.felipejanser.cursomc.services.DBService;
+import com.felipejanser.cursomc.services.EmailService;
+import com.felipejanser.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -28,6 +30,11 @@ public class DevConfig {
 
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() throws Exception {
+		return new SmtpEmailService();
 	}
 	
 }
--- src/main/java/com/felipejanser/cursomc/services/DBService.java
+++ src/main/java/com/felipejanser/cursomc/services/DBService.java
@@ -112,7 +112,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3,c4,c5));
 		
-		Cliente cli1 = new Cliente(null,"Maria Silva","maria@gmail.com","09574845427", TipoCliente.PESSOAFISICA); 
+		Cliente cli1 = new Cliente(null,"Maria Silva","felipejanser@gmail.com","09574845427", TipoCliente.PESSOAFISICA); 
 		Cliente cli2 = new Cliente(null,"João Silva","joao@gmail.com","98765432127", TipoCliente.PESSOAFISICA);
 		
 		cli1.getTelefones().addAll(Arrays.asList("1234","5678"));
