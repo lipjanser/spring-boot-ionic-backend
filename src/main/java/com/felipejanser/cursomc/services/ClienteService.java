bc29102d78a02f92b0bb349ceae67ec8abe75b45
Felipe Janser <felipejanser@gmail.com>
2020-05-25 13:57:48 -0300

    Enviando imagem via endpoint

--- src/main/java/com/felipejanser/cursomc/services/ClienteService.java
+++ src/main/java/com/felipejanser/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.felipejanser.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.felipejanser.cursomc.domain.Cidade;
 import com.felipejanser.cursomc.domain.Cliente;
@@ -31,10 +33,15 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private S3Service s3service;
 	
 	public Cliente find(Integer id) {
 		
@@ -103,6 +110,10 @@ public class ClienteService {
 	private void updateData(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3service.uploadFile(multipartFile);
 	}
 	
 }
--- src/main/java/com/felipejanser/cursomc/services/S3Service.java
+++ src/main/java/com/felipejanser/cursomc/services/S3Service.java
@@ -1,16 +1,19 @@
 package com.felipejanser.cursomc.services;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 
 @Service
@@ -24,18 +27,28 @@ public class S3Service {
 	@Value("${s3.bucket}")
 	private String s3Bucket;
 
-	public void uploadFile(String localFilePath) {
+	public URI uploadFile(MultipartFile multipartFile) {
 		try {
-			File file = new File(localFilePath);
-			String[] arFilePath = localFilePath.split("\\\\");
+			String fileName = multipartFile.getOriginalFilename();
+			InputStream is = multipartFile.getInputStream();
+			String contentType = multipartFile.getContentType();
+			return uploadFile(is, fileName, contentType);
+		} catch (IOException e) {
+			throw new RuntimeException("Erro de IO: " + e.getMessage());
+		}
+
+	}
+
+	public URI uploadFile(InputStream is, String fileName, String contentType) {
+		try {
+			ObjectMetadata meta = new ObjectMetadata();
+			meta.setContentType(contentType);
 			LOG.info("Iniciando upload...");
-			s3client.putObject(new PutObjectRequest(s3Bucket, arFilePath[arFilePath.length - 1], file));
+			s3client.putObject(new PutObjectRequest(s3Bucket, fileName, is, meta));
 			LOG.info("Upload realizado...");
-		} catch (AmazonServiceException e) {
-			LOG.info("AmazonServiceException: " + e.getErrorMessage());
-			LOG.info("Status Code: " + e.getErrorCode());
-		} catch (AmazonClientException e) {
-			LOG.info("AmazonClientException: " + e.getMessage());
+			return s3client.getUrl(s3Bucket, fileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Erro ao converter URL para URI.");
 		}
 	}
 
