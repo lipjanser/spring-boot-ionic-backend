f8472d9fadb76b6e5cb6e378de064d64e94870b6
Felipe Janser <felipejanser@gmail.com>
2020-05-25 11:42:18 -0300

    Salvando primeiro arquivo no S3

---
+++ src/main/java/com/felipejanser/cursomc/config/S3Config.java
@@ -0,0 +1,35 @@
+package com.felipejanser.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+@Configuration
+public class S3Config {
+	
+	@Value("${aws.access_key_id}")
+	private String awsID;
+	
+	@Value("${aws.secret_access_key}")
+	private String awsSecretKey;
+	
+	@Value("${s3.region}")
+	private String s3Region;
+
+	@Bean
+	public AmazonS3 s3Client() {
+		BasicAWSCredentials awsCred = new BasicAWSCredentials(awsID, awsSecretKey);
+		AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+				.withRegion(Regions.fromName(s3Region))
+				.withCredentials(new AWSStaticCredentialsProvider(awsCred)).build();
+		
+		return s3client;
+	}
+	
+}
